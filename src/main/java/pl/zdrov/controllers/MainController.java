e1ae70baedc0bf2f7f9ce77201555a4443e71a4c
Bartłomiej Kotarski <bkotarski1@gmail.com>
2019-04-07 14:25:31 +0200

    Added MainControler for MainFXML

---
+++ src/main/java/pl/zdrov/controllers/MainController.java
@@ -0,0 +1,15 @@
+package pl.zdrov.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+public class MainController {
+    @FXML
+    public AnchorPane anchorPane;
+
+    @FXML
+    public void initialize()
+    {
+
+    }
+}
